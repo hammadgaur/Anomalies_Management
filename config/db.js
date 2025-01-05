cdd2940b2eba6c18ff70c1fc47ee8104296aca02
Mohd Hammad <37648155+hammadgaur@users.noreply.github.com>
2025-01-05 15:23:51 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
-import mysql from 'mysql2';
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'FormDB',
-    port: process.env.DB_PORT || 3306,
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-export default pool.promise();
-
+export default db;
